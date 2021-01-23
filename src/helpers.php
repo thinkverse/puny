c210821dad7953494e91c189bdc1729b6120defb
thinkverse <hallberg.kim@gmail.com>
2021-01-23 14:56:52 +0100

    feat: eq()

--- src/helpers.php
+++ src/helpers.php
@@ -26,6 +26,28 @@ function spy(callable $callback) {
     return new Spy($callback);
 }
 
+/**
+ * @param mixed $expected
+ * @param mixed $actual
+ *
+ * @return bool
+ */
+function eq($expected, $actual) {
+    if ($expected === $actual) {
+        return true;
+    }
+
+    if (is_callable($expected)) {
+        $expected = (\Closure::fromCallable($expected))();
+    }
+
+    if (is_callable($actual)) {
+        $actual = (\Closure::fromCallable($actual))();
+    }
+
+    return $expected == $actual;
+}
+
 function skip() {
     throw new SkippedException;
 }
---
+++ tests/eq.php
@@ -0,0 +1,19 @@
+<?php
+
+use function Puny\{test, eq, ok};
+
+test('The `eq` function works', function () {
+    ok(eq(2, 1 + 1), '`eq` works.');
+});
+
+test('The `eq` function works with array', function () {
+    ok(eq([2], [1 + 1]), '`eq` works.');
+});
+
+test('The `eq` function works with callable', function () {
+    ok(eq(fn () => 2, fn () => 1 + 1), '`eq` works.');
+});
+
+test('The `eq` function works with function', function () {
+    ok(eq(ceil(1.2), 2), '`eq` works.');
+});
